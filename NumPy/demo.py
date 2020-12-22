eddd9e658bf88ab5b304f28eb3055152a17c3647
wbytts <wbytts@163.com>
2020-12-23 03:51:07 +0800

    Turtle 绘制小樱魔法阵

--- NumPy/demo.py
+++ NumPy/demo.py
@@ -4,4 +4,5 @@ import time
 
 A = np.array([1, 2, 3, 4])
 A2 = list(map(lambda x: x+1, A))
+
 print(A2)
---
+++ pickle/001-基本使用.py
@@ -0,0 +1,8 @@
+import pickle
+
+m = [1, 2, 3, 4, 5]
+
+# 将Python对象序列化为二进制串
+res = pickle.dumps(m)
+print(res)
+
