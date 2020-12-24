a608c0b7091b6038d7e3426711eba7157910baa3
wbytts <wbytts@163.com>
2020-12-24 21:00:37 +0800

    update 20-12-24

---
+++ unittest/demo.py
@@ -0,0 +1,14 @@
+import unittest
+
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_add(self):
+        print(1+1)
+
+    def test_minus(self):
+        print(3-4)
+
+
+if __name__ == '__main__':
+    unittest.main()
